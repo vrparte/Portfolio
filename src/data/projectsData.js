6a50ee2d856b5ee1361737731661df375ad7af5e
vrparte <partevr@mail.uc.edu>
2024-03-19 00:19:47 -0400

    Removed extra project titles

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,11 +1,11 @@
 import one from '../assets/svg/projects/one.svg'
 import two from '../assets/svg/projects/two.svg'
 import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
+//import four from '../assets/svg/projects/four.svg'
+//import five from '../assets/svg/projects/five.svg'
+//import six from '../assets/svg/projects/six.svg'
+//import seven from '../assets/svg/projects/seven.svg'
+//import eight from '../assets/svg/projects/eight.svg'
 
 
 export const projectsData = [
@@ -14,8 +14,8 @@ export const projectsData = [
         projectName: 'HR Management',
         projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
         tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://github.com/vrparte',
+        demo: 'https://github.com/vrparte',
         image: one
     },
     {
@@ -23,8 +23,8 @@ export const projectsData = [
         projectName: 'Railway Tracking',
         projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
         tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: two
     },
     {
@@ -32,17 +32,17 @@ export const projectsData = [
         projectName: 'Weather App',
         projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
         tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: three
     },
-    {
+    /*{
         id: 4,
         projectName: 'Android Patient Tracker',
         projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
         tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: four
     },
     {
@@ -50,8 +50,8 @@ export const projectsData = [
         projectName: 'E-Commerce App',
         projectDesc: 'A Simple E-commerce application',
         tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: five
     },
     {
@@ -59,8 +59,8 @@ export const projectsData = [
         projectName: 'Uber Lite',
         projectDesc: 'Uber clone',
         tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: six
     },
     {
@@ -68,8 +68,8 @@ export const projectsData = [
         projectName: 'Stock Market App',
         projectDesc: 'A simple stock market API app',
         tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: seven
     },
     {
@@ -77,10 +77,10 @@ export const projectsData = [
         projectName: 'Car Pooling System',
         projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
         tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        code: 'https://https://github.com/vrparte',
+        demo: 'https://https://github.com/vrparte',
         image: eight
-    },
+    },*/
 ]
 
 
